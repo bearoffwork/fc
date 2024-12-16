895c2201191f5e9bb398f6aa2ef5175da0653f2f
Bear.Y <codes@rra3b.io>
2024-12-16 19:07:55 +0800

    repeater

--- app/Filament/Resources/CustomerResource.php
+++ app/Filament/Resources/CustomerResource.php
@@ -4,20 +4,25 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\CustomerResource\Pages;
 use App\Models\Customer;
+use App\Models\Probe;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use function Laravel\Prompts\text;
+use Illuminate\Support\HtmlString;
+use Illuminate\Support\Str;
 
 class CustomerResource extends Resource
 {
     protected static ?string $model = Customer::class;
 
     protected static ?string $navigationLabel = '客戶管理';
+
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
@@ -30,8 +35,30 @@ class CustomerResource extends Resource
                         TextInput::make('company_name'),
                         TextInput::make('company_phone'),
                         TextInput::make('company_address'),
+                        Select::make('probes')
+                            ->relationship()
+                            ->allowHtml()
+                            ->getOptionLabelFromRecordUsing(fn(Probe $record) => new HtmlString("
+                                <div>{$record->probe_id}</div>
+                                <div>{$record->type}</div>
+                            "))
+                            ->multiple()
+                            ->preload()
+                            ->default(null)
+                            ->createOptionForm([
+                                TextInput::make('probe_id')
+                                    ->default(Str::random()),
+                                TextInput::make('type')
+                                    ->required(),
+                            ]),
+                        // Repeater::make('probe')
+                        //     ->relationship('probes')
+                        //     ->schema([
+                        //         TextInput::make('probe_id')->default(Str::random()),
+                        //         TextInput::make('type'),
+                        //     ]),
                     ])
-                    ->columns(3)
+                    ->columns(3),
             ]);
     }
 
